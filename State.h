9e36612e96a9714f334bed00f268aee93bd445e3
ld18 <42042383+ld18@users.noreply.github.com>
2022-01-07 21:57:13 +0100

    Update State.h

--- State.h
+++ State.h
@@ -6,7 +6,7 @@
 using std::string;
 using std::chrono::high_resolution_clock;
 
-/// <value> Action a state can performe, see UML entry, do and exit actions </value>
+/// <value> Action a state can performe, see UML entry, do and exit actions. </value>
 enum StateAction {
 	enter,
 	stay,
@@ -19,9 +19,10 @@ class State {
 public:
 	State() = default;
 	
-	/// <summary> Get the next action from a state. 
-	///Check the conditions and calculate if the machine enters into a state, stay in the current one or leaves the current one. </summary>
-	/// <returns> StateAction: indicates which action will be performed </returns>  
+	/// <summary> Get the next action from a state and the next state. 
+	/// Check the conditions and calculate if the machine enters into a state, stay in the current one or leaves the current one. </summary>
+	/// <param name="newState"> Used to insert the new State, only useed when leaving the state. </param>
+	/// <returns> Indicates which action will be performed. </returns>  
 	/// <remarks> Need to be fully rewritten by you. </remarks>
 	virtual StateAction getNextAction(State*& newState);
 	
@@ -38,14 +39,14 @@ public:
 	string name = "baseState";
 
 	/// <value> Timepoint when the current state was enterd. </value>
-	/// <remarks> Should be set in doEnterActions() </remarks>
+	/// <remarks> Should be set in doEnterActions(). </remarks>
 	high_resolution_clock::time_point enteredAt;
 	
 	/// <value> Timepoint when the last state was left. </value>
-	/// <remarks> Should be set in doLeaveActions() </remarks>
+	/// <remarks> Should be set in doLeaveActions(). </remarks>
 	high_resolution_clock::time_point leaftAt;
 protected:
 	/// <value> Internal boolean to differentiate the enter action from a stay action. </value>
-	/// <remarks> Have a look into State::getNextAction() </remarks>
+	/// <remarks> Have a look into State::getNextAction(). </remarks>
 	bool entered = false;
 };
